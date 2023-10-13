cb6a2ffe2d650777fee45a8b75b1104a1027b661
Carl0120 <carlosmichelcasanova@gmail.com>
2023-10-13 11:39:11 -0400

    Configured Entities

---
+++ src/main/java/com/project/model/entity/TypeProduct.java
@@ -0,0 +1,31 @@
+package com.project.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TypeProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Override
+    public String toString() {
+        return "TypeProduct{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
